d02f9627e5e148288c0d471f6a47ea9894688f74
Solitary Cloud <lessonxiao@qq.com>
2021-05-26 22:39:47 +0800

    master

---
+++ lesson02/build/CMakeFiles/lesson02_exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson02_exe.dir/src/SeqList.c.o"
+  "CMakeFiles/lesson02_exe.dir/src/main.c.o"
+  "lesson02_exe"
+  "lesson02_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lesson02_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
