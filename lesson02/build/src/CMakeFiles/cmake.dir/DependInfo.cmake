f11d26db6508fd3d8e197d34bc3e6b5a89c58860
Be-xiao <lessonxiao@qq.com>
2021-05-26 15:45:02 +0800

    master

---
+++ lesson02/build/src/CMakeFiles/cmake.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/noah/Documents/.code/Data-Structures-and-Algorithms/lesson02/src/Seqlist.c" "/home/noah/Documents/.code/Data-Structures-and-Algorithms/lesson02/build/src/CMakeFiles/cmake.dir/Seqlist.c.o"
+  "/home/noah/Documents/.code/Data-Structures-and-Algorithms/lesson02/src/test.c" "/home/noah/Documents/.code/Data-Structures-and-Algorithms/lesson02/build/src/CMakeFiles/cmake.dir/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lesson02/build/src/CMakeFiles/cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake.dir/Seqlist.c.o"
+  "CMakeFiles/cmake.dir/test.c.o"
+  "cmake"
+  "cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
