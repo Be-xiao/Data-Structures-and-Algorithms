f9901ae30b3b3c22a8daa471c2d6317be7496d65
Be-xiao <lessonxiao@qq.com>
2021-05-27 14:27:13 +0800

    master

--- lesson02/build/cmake_install.cmake
+++ lesson02/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/noah/Documents/code/Data-Structures-and-Algorithms/lesson02
+# Install script for directory: /home/noah/Documents/.code/Data-Structures-and-Algorithms-1/lesson02
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/noah/Documents/code/Data-Structures-and-Algorithms/lesson02/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/noah/Documents/.code/Data-Structures-and-Algorithms-1/lesson02/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
